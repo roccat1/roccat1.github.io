6254914f43971918a2a1ed9b13e3aafd67e9da0b
roccat1 <roc.r2005@gmail.com>
2021-11-28 19:21:45 +0100

    bugged

---
+++ tictactoe/js.js
@@ -0,0 +1,13 @@
+const box11 = document.getElementById("box-11");
+const box12 = document.getElementById("box-12");
+const box13 = document.getElementById("box-13");
+const box21 = document.getElementById("box-21");
+const box22 = document.getElementById("box-22");
+const box23 = document.getElementById("box-23");
+const box31 = document.getElementById("box-31");
+const box32 = document.getElementById("box-32");
+const box33 = document.getElementById("box-33");
+
+const box11Click ={console.log("a");}
+
+box11.onclick = box11Click();
