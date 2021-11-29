73f9cb26992428286d1ace7d7ff538aa02aab071
roccat1 <roc.r2005@gmail.com>
2021-11-29 17:42:02 +0100

    main changes and tictactoe added

--- tictactoe/js.js
+++ tictactoe/js.js
@@ -1,34 +1,86 @@
-const box11 = document.getElementById("box-11");
-const box12 = document.getElementById("box-12");
-const box13 = document.getElementById("box-13");
-const box21 = document.getElementById("box-21");
-const box22 = document.getElementById("box-22");
-const box23 = document.getElementById("box-23");
-const box31 = document.getElementById("box-31");
-const box32 = document.getElementById("box-32");
-const box33 = document.getElementById("box-33");
+//first, we identify the boxes
+const box0 = document.getElementById("box-0");
+const box1 = document.getElementById("box-1");
+const box2 = document.getElementById("box-2");
+const box3 = document.getElementById("box-3");
+const box4 = document.getElementById("box-4");
+const box5 = document.getElementById("box-5");
+const box6 = document.getElementById("box-6");
+const box7 = document.getElementById("box-7");
+const box8 = document.getElementById("box-8");
 
+//make some var
+var states = [0,0,0,0,0,0,0,0,0]
 var turn = 0;
 
-function clicked(who){
-    console.log(who.target.src);
-    if(turn == 0){
-        console.log("b");
-        who.target.src = "O.png"
-        console.log(who.clicked)
-    } else if(turn == 1){
-        console.log("c");
+//function when you win as argument the state that won
+function win(who){
+    if(who == 10){
+        console.log("O won");
     } else{
-        console.error("Error 001");
+        console.log("X won");
     }
 }
 
-box11.onclick = clicked;
-box12.onclick = clicked;
-box13.onclick = clicked;
-box21.onclick = clicked;
-box22.onclick = clicked;
-box23.onclick = clicked;
-box31.onclick = clicked;
-box32.onclick = clicked;
-box33.onclick = clicked;
+//function to check if you are wining
+function checkWin(){
+    //check if the states are in line and they are diferent of 0
+    if(states[0] == states[1] && states[1] == states[2] && states[2] != 0){
+        win(states[0]);
+    } else if(states[3] == states[4] && states[4] == states[5] && states[5] != 0){
+        win(states[3]);
+    } else if(states[6] == states[7] && states[7] == states[8] && states[8] != 0){
+        win(states[6]);
+    } else if(states[0] == states[3] && states[3] == states[6] && states[6] != 0){
+        win(states[0]);
+    } else if(states[1] == states[4] && states[4] == states[7] && states[7] != 0){
+        win(states[1]);
+    } else if(states[2] == states[5] && states[5] == states[8] && states[8] != 0){
+        win(states[2]);
+    } else if(states[0] == states[4] && states[4] == states[8] && states[8] != 0){
+        win(states[0]);
+    } else if(states[2] == states[4] && states[4] == states[6] && states[6] != 0){
+        win(states[2]);
+    }
+    //you're not wining :(
+    else{
+        console.log("not wining")
+    }
+}
+
+//someone clicked, argument of who clicked
+function clicked(who){
+    //who.target.id[4] -- num picked
+    if(turn == 0){
+        if(states[who.target.id[4]] == 0){
+            who.target.src = "O.png";
+            states[who.target.id[4]] = 10;
+            turn = 1;
+        } else{
+            console.log("alrady picked")
+        }
+    } else if(turn == 1){
+        if(states[who.target.id[4]] == 0){
+            who.target.src = "X.png";
+            states[who.target.id[4]] = 1;
+            turn = 0;
+        } else{
+            console.log("alrady picked")
+        }
+    } else{
+        console.error("Error 001");
+    }
+    checkWin();
+}
+
+
+//eventListeners preparation
+box1.onclick = clicked;
+box2.onclick = clicked;
+box3.onclick = clicked;
+box4.onclick = clicked;
+box5.onclick = clicked;
+box6.onclick = clicked;
+box7.onclick = clicked;
+box8.onclick = clicked;
+box0.onclick = clicked;
